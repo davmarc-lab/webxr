463c3d08150fbcccc0c9b288bae8716c386e823f
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2026-01-13 15:54:51 +0100

    retrieve camera image with webxr, create a texture and render on a cube

--- aruco-video/index.js
+++ aruco-video/index.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import { ARButton } from 'three/addons/webxr/ARButton.js';
+
 import { AR } from 'js-aruco2'
 
 import * as Utils from './utils.js'
@@ -21,27 +23,6 @@ const FAR = 100;
 
 const maxPixelCount = 3840 * 2160;
 
-const video = document.getElementById("video");
-video.hidden = true;
-
-// get user camera stream if available
-const enableVideo = navigator.mediaDevices && navigator.mediaDevices.getUserMedia;
-// const enableVideo = false;
-if (enableVideo) {
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            facingMode: "environment"
-        }
-    }).then(stream => {
-        video.srcObject = stream;
-        video.play();
-        video.onplay = _ => {
-            video.classList.add("visible");
-        };
-    });
-}
-
 // aruco detector
 const detector = new AR.Detector({
     dictionaryName: "ARUCO"
@@ -52,6 +33,12 @@ let tracked = [];
 const canvas = document.getElementById("scene");
 
 const renderer = new THREE.WebGLRenderer({ antialias: false, canvas: canvas });
+renderer.xr.enabled = true;
+let xrBinding = null;
+
+document.body.appendChild(ARButton.createButton(renderer, {
+    requiredFeatures: ["camera-access"]
+}));
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, NEAR, FAR);
@@ -60,8 +47,7 @@ camera.position.z = 5;
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-cube.position.z = -2;
-cube.position.x = 17.5;
+cube.position.z = -3;
 scene.add(cube);
 
 canvas.addEventListener('resize', resizeRenderer());
@@ -89,69 +75,63 @@ function resizeRenderer() {
     }
 }
 
-function addMarkerToTracked(marker) {
-    // add the current marker to tracked markers
-    if (tracked.length == 0 && tracked.find(t => t.marker.id === marker.id) === undefined) {
-        tracked.push(new RMarker(marker));
-    }
+const cameraScene = new THREE.Scene();
+const cameraMat = new THREE.SpriteMaterial({ color: 0x00ff00 });
+const cameraSprite = new THREE.Sprite(cameraMat);
+cameraSprite.scale.x = 3;
+cameraSprite.scale.y = 3;
+cameraScene.add(cameraSprite);
 
-    let markerSize = 35.0; //millimeters
-    const pos = new POS.Posit(markerSize, canvas.width);
-    let corners = marker.corners;
-
-    // center corners
-    for (let i = 0; i < corners.length; ++i) {
-        let corner = corners[i];
-
-        corners[i].x = corner.x - (canvas.width / 2);
-        corners[i].y = (canvas.height / 2) - corner.y;
-    }
-    const pose = pos.pose(corners);
-    const t = pose.bestTranslation;
-    const T = pose.alternativeTranslation;
-    log(t[0] + ", " + t[1]);
-    log(T[0] + ", " + T[1]);
-
-    // const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-    // const points = marker.corners.map(c => new THREE.Vector3(c.x, c.y, 1));
-    // const geom = new THREE.BufferGeometry().setFromPoints(points);
-    // const line = new THREE.Line(geom, material);
-    // scene.add(line);
-}
-
+let first = true;
 function render(time) {
-    // render the webcam video as scene background
-    let videoImage;
-    if (enableVideo) {
-        videoImage = Utils.videoSnapshot(video);
-        scene.background = videoImage;
-        scene.background.needsUpdate = true;
-    } else {
-        videoImage = new THREE.TextureLoader().load("marker.png", tex => {
-            scene.background = tex;
-            scene.background.needsUpdate = true;
-        });
-    }
-
-    if (renderer.info.render.frame % 100 == 0) {
-        // retrieve the current render target
-        const oldRt = renderer.getRenderTarget();
-        // retrieve the image data of the current scene state
-        // render on a different render target the scene content (camera as background)
-        const imageData = Utils.snapshot(renderer, camera, scene);
-        // restore the old render target
-        renderer.setRenderTarget(oldRt);
-
-        // detect aruco markers
-        const markers = detector.detect(imageData);
-        console.log("Markers found: " + markers.length);
-        log("Markers found: " + markers.length);
-
-        markers.forEach(m => addMarkerToTracked(m));
-    }
-
     // render scene
     renderer.render(scene, camera);
+
+    if (renderer.info.render.frame % 100 == 0) {
+        if (first) {
+            first = false;
+            return;
+        }
+        // update camera sprite texture
+        let texture;
+        const frame = renderer.xr.getFrame();
+        const rf = renderer.xr.getReferenceSpace();
+        if (frame && rf) {
+            const viewPose = frame.getViewerPose(rf);
+            if (viewPose) {
+                const view = viewPose.views[0].camera;
+                texture = renderer.xr.getCameraTexture(view);
+                cameraSprite.material.map = texture;
+                cameraSprite.material.needsUpdate = true;
+
+                //-------ERROR-------
+                // texture is ok, there could be a problem when rendering on another render target
+                // (maybe sprite size is too small)
+                // - try render only the camera scene
+
+                // retrieve the current render target
+                const oldRt = renderer.getRenderTarget();
+                // retrieve the image data of the current scene state
+                // render on a different render target the scene content (camera as background)
+                const imageData = Utils.snapshot(renderer, camera, cameraScene);
+                // restore the old render target
+                renderer.setRenderTarget(oldRt);
+
+                // detect aruco markers
+                if (texture !== undefined) {
+                    // debug camera image
+                    cube.material.map = texture;
+                    cube.material.needsUpdate = true;
+
+                    const markers = detector.detect(imageData);
+                    log("Markers found: " + markers.length);
+                } else {
+                    log("SKIPPING")
+                }
+            }
+        }
+
+    }
 }
 
 function loop(time) {
