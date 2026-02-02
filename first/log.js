e3e9411ff59ce7349e8f44b02a6a8d5358603dcd
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2026-02-02 11:40:17 +0100

    removed old projects

--- first/log.js
+++
@@ -1,9 +0,0 @@
-function log(message) {
-    fetch("/logs", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: message })
-    })
-}
-
-export { log }
